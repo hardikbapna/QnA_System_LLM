52775bb4cd33ab9aee304cb8169e3e805583af27
hardikbapna <hardikbapna@gmail.com>
2023-06-14 00:16:52 +0530

    removing unnecessary files for security.

--- pinecone_functionality.py
+++
@@ -1,7 +0,0 @@
-def get_similar_docs(query, k, score= False):
-  if score:
-    similar_docs= index.similarity_search_with_score(query=query, k=k)
-  else:
-    similar_docs= index.similarity_search(query=query, k=k)
-
-  return similar_docs
